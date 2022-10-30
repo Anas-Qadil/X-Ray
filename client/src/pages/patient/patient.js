00943c4f6bbdf19c48c09060c33dbf29a05346fb
Anas Qadil <qadilanass10@gmail.com>
2022-10-30 22:09:57 +0100

    update

--- client/src/pages/patient/patient.js
+++ client/src/pages/patient/patient.js
@@ -29,7 +29,6 @@ const Patient = () => {
       if (res.status === 200) {
         if (res.data.lastyearDose >= 18)
           enqueueSnackbar("You Have exceeded The Does Rate Limit.", {variant: 'warning'})
-        console.log(res.data);
         formatData(res?.data?.data);
         setDoseData(res.data);
         setDataLoading(false);
